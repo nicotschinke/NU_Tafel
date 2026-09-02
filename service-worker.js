ccbd70599c3678556fe431a842116b1c1e72f47c
Nico Tschinke <tschinke.nico@gmail.com>
2026-09-02 14:37:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "NU_Tafel";
 const ASSETS_TO_CACHE = [
-  "NU_Tafel/images/NU_atlas_1.webp",
-  "NU_Tafel/images/EW_atlas_3.webp",
-  "NU_Tafel/createjs.min.js",
-  "NU_Tafel/index.html"
+  "/NU_Tafel/images/NU_atlas_1.webp",
+  "/NU_Tafel/images/EW_atlas_3.webp",
+  "/NU_Tafel/createjs.min.js",
+  "/NU_Tafel/index.html"
 ];
 
 self.addEventListener("install", (event) => {
