5709454dd13856ad1645b3793afef2c22aad7679
Nico Tschinke <tschinke.nico@gmail.com>
2026-09-02 14:34:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = "NU_Tafel";
 const ASSETS_TO_CACHE = [
-  "/NU_Tafel/images/NU_atlas_1.webp",
-  "/NU_Tafel/images/EW_atlas_3.webp",
-  "/NU_Tafel/createjs.min.js",
+  "NU_Tafel/images/NU_atlas_1.webp",
+  "NU_Tafel/images/EW_atlas_3.webp",
+  "NU_Tafel/createjs.min.js",
   "NU_Tafel/index.html"
 ];
 
